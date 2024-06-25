9955db34367010c0ed134e623aacf68d45bb5b59
Iryna P <irpolg@ukr.net>
2024-06-25 21:27:16 +0300

    8hw-0init

---
+++ src/redux/filters/slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialFilterState = { name: "" };
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: initialFilterState,
+    reducers: {
+        changeFilter(state, {payload}) {
+            state.name = payload
+        }
+    }
+});
+
+export const { changeFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
+export const selectNameFilter = state => state.filter.name;
